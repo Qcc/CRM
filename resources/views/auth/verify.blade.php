5ccea5496e180257d53a9490781275a0e0b22bc5
kevin <kevin.e@qq.com>
2019-05-05 17:31:32 +0800

    邮箱验证

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,22 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
-                </div>
+<div class="layui-row">
+        <div class="layui-col-md4 layui-col-md-offset4">
+          
+        <div class="layui-card">
+            <div class="layui-card-header">{{ __('Verify Your Email Address') }}</div>
+            <div class="layui-card-body">
+                @if (session('resent'))
+                <blockquote class="layui-elem-quote">
+                    {{ __('A fresh verification link has been sent to your email address.') }}
+                </blockquote>
+                @endif
+                {{ __('Before proceeding, please check your email for a verification link.') }}
+                {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
             </div>
         </div>
     </div>
