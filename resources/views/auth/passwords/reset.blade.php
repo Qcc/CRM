8f4d8d464f40d8acd0ff1eea587166853afd4612
kevin <kevin.e@qq.com>
2019-05-07 13:40:45 +0800

    创建models

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -11,47 +11,37 @@
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="layui-form-item">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
-
+                                <label class="layadmin-user-login-icon layui-icon layui-icon-username" for="LAY-user-login-username"></label>
+                                <input type="text" name="email" id="LAY-user-login-username" lay-verify="required" placeholder="{{ __('E-Mail Address') }}" class="layui-input{{ $errors->has('email') ? ' layui-form-danger' : '' }}">
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
                         <div class="layui-form-item">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
+                                <label class="layadmin-user-login-icon layui-icon layui-icon-password" for="LAY-user-reg-password"></label>
+                                <input type="password" name="password" id="LAY-user-reg-password" lay-verify="required" placeholder="{{ __('Password') }}" class="layui-input{{ $errors->has('password') ? ' layui-form-danger' : '' }}">
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
                         <div class="layui-form-item">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
+                                <label class="layadmin-user-login-icon layui-icon layui-icon-password" for="LAY-conf-reg-password"></label>
+                                <input type="password" name="password_confirmation" id="LAY-conf-reg-password" lay-verify="required" placeholder="重复密码" class="layui-input{{ $errors->has('password_confirmation') ? ' layui-form-danger' : '' }}">
+                                @if ($errors->has('password_confirmation'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
-                        <div class="layui-form-item mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
+                        <div class="layui-form-item">
+                            <button class="layui-btn layui-btn-fluid" lay-submit="" lay-filter="LAY-user-login-submit">{{ __('Reset Password') }}</button>
                         </div>
                     </form>
                 </div>
